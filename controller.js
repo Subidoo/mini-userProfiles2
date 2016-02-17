d9844fb7714413b2ed939cd6b6e684c432f483b9
Kyle Prestgard <ktprestgard@gmail.com>
2016-02-16 19:35:30 -0700

    fixed stuff

--- controller.js
+++ controller.js
@@ -1,10 +1,23 @@
 var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
+  
+  $scope.currentPage = 1;
+
   $scope.getUsers = function() {
-    mainService.getUsers().then(function(response) {
-      $scope.users = response.data.data;
+    mainService.getUsers($scope.currentPage).then(function(response) {
+      $scope.users = response.data;
     });
   }
   $scope.getUsers();
+
+  $scope.next = function() {
+  	$scope.currentPage++;
+  	$scope.getUsers();
+  }
+
+   $scope.previous = function() {
+  	$scope.currentPage--;
+  	$scope.getUsers();
+  }
 });
--- service.js
+++ service.js
@@ -1,10 +1,14 @@
 var app = angular.module('userProfiles');
 
 app.service('mainService', function($http) {
-  this.getUsers = function() {
+  this.getUsers = function(page) {
     return $http({
       method: 'GET',
-      url: 'http://reqres.in/api/users?page=1'
-    });
+      url: 'http://reqres.in/api/users?page=' + page,
+    }).then(function(response) {
+        if(response.status === 200){
+            return response.data;
+        }
+    })
   }
 });
